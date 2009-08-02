f237ca6d55a93edac6ef2b8ab0ddcc31e370a252
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2009-08-02 06:57:24 +0200

    "Update to version 2.0.5"

--- bookmark+.el
+++ bookmark+.el
@@ -180,7 +180,7 @@
 (defvar tramp-file-name-regexp)         ; Defined in `tramp.el'.
 (defvar bookmark-make-record-function)  ; Defined in `bookmark.el'.
 
-(defconst bookmark+version-number "2.0.4")
+(defconst bookmark+version-number "2.0.5")
 
 (defun bookmark+version ()
   "Show version number of bookmark+.el"
