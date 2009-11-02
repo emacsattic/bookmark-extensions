072cea8d3f71b57d3c2115846aaed570d896f557
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2009-11-02 19:22:44 +0100

    Fix title in bmkext-bmenu-search.

--- bookmark-extensions.el
+++ bookmark-extensions.el
@@ -299,7 +299,7 @@
 (require 'bookmark)
 (eval-when-compile (require 'cl))
 
-(defconst bmkext-version-number "2.6.4")
+(defconst bmkext-version-number "2.6.5")
 
 (defun bmkext-version ()
   "Show version number of library `bookmark-extensions.el'."
@@ -1534,8 +1534,9 @@ If a prefix arg is given search in the whole `bookmark-alist'."
     (lexical-let* ((ctitle   (save-excursion (goto-char (point-min))
                                              (buffer-substring (point-at-bol) (point-at-eol))))
                    (ntitle   "% Bookmark Filtered by regexp")
-                   (bmk-list (if all
-                                 (prog1 bookmark-alist (setq ntitle "% Bookmark"))
+                   (bmk-list (if all    ; Prefix arg
+                                 (prog1 bookmark-alist (setq ntitle "% Bookmark"
+                                                             ctitle "% Bookmark"))
                                  bmkext-latest-bookmark-alist)))
       (unwind-protect
            (progn
@@ -1545,11 +1546,12 @@ If a prefix arg is given search in the whole `bookmark-alist'."
                     #'(lambda ()
                         (bmkext-bmenu-filter-alist-by-regexp bmkext-search-pattern bmk-list ntitle))))
              (bmkext-read-search-input))
-        (if bmkext-signal-quit
+        (if bmkext-signal-quit          ; C-g hit, rebuild alist as before.
             (let ((bookmark-alist bmk-list)
                   (bmkext-bmenu-called-from-inside-flag t))
               (bookmark-bmenu-list ctitle)
               (bmkext-bmenu-cancel-search))
+            ;; Else show the narrowed alist only.
             (message "%d bookmarks found matching `%s'"
                      (length bmkext-latest-bookmark-alist) bmkext-search-pattern)
             (bmkext-bmenu-cancel-search))))))
