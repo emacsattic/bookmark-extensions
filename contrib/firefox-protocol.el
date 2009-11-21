0f8f7de2ecbd1e727e1213c8af704f09196a0913
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2009-11-21 16:26:49 +0100

    firefox-protocol.el: Find default firefox user directory automatically

--- contrib/firefox-protocol.el
+++ contrib/firefox-protocol.el
@@ -40,101 +40,115 @@
 
 (eval-when-compile (require 'cl))
 
-(defvar firefox-default-user-file "~/labo/test-proto/user.js")
-(defvar firefox-default-mimeTypes-file "~/labo/test-proto/mimeTypes.rdf")
+(defun firefox-protocol-get-user-init-dir ()
+  (let* ((moz-dir (concat (getenv "HOME") "/.mozilla/firefox/"))
+         (moz-user-dir (with-current-buffer (find-file-noselect (concat moz-dir "profiles.ini"))
+                         (goto-char (point-min))
+                         (when (search-forward "Path=" nil t)
+                           (buffer-substring-no-properties (point) (point-at-eol))))))
+    (file-name-as-directory (concat moz-dir moz-user-dir))))
+         
+(defvar firefox-default-user-file "user.js")
+(defvar firefox-default-mimeTypes-file "mimeTypes.rdf")
 
 (defun firefox-protocol-handler-backup ()
-  (let ((new-mimeType-fname (symbol-name
-                             (gensym (file-name-sans-extension firefox-default-mimeTypes-file))))
+  (let ((mimeType-fname (concat (firefox-protocol-get-user-init-dir)
+                                firefox-default-mimeTypes-file))
+        (user-fname     (concat (firefox-protocol-get-user-init-dir)
+                                firefox-default-user-file))
+        (new-mimeType-fname (symbol-name
+                             (gensym (file-name-sans-extension mimeType-fname))))
         (new-fs-file-fname (symbol-name
-                            (gensym (file-name-sans-extension firefox-default-user-file)))))
-    (copy-file firefox-default-mimeTypes-file
-               (concat new-mimeType-fname ".rdf"))
-    (copy-file firefox-default-user-file
-               (concat new-fs-file-fname ".js"))))
+                            (gensym (file-name-sans-extension user-fname)))))
+    (when (file-exists-p firefox-default-mimeTypes-file)
+      (copy-file mimeType-fname (concat new-mimeType-fname ".rdf")))
+    (when (file-exists-p firefox-default-user-file)    
+    (copy-file user-fname (concat new-fs-file-fname ".js")))))
     
 (defun firefox-protocol-installer-install (name path)
   (interactive "sProtocolName: \nsHandlerFileName: ")
   (when (y-or-n-p "Backup original files?")
     (firefox-protocol-handler-backup))
-  (when (and firefox-default-mimeTypes-file
-             firefox-default-user-file)
-    (let ((regexp-start-section-js  (format "// Section %s" name))
-          (regexp-end-section-js    (format "// End Section %s" name))
-          (regexp-start-section-rdf (format "<!-- Section %s -->" name))
-          (regexp-end-section-rdf   (format "<!-- End Section %s -->" name)))
-      ;; Write new protocol or replace an old entry to user.js file.
-      (with-current-buffer (find-file-noselect firefox-default-user-file)
-        (goto-char (point-min))
-        ;; If entry already exists, remove it and replace by new one.
-        ;; Else we go at end of buffer and add new entry.
-        (if (re-search-forward regexp-start-section-js nil t)
-            (let ((beg (point-at-bol))
-                  (end (save-excursion
-                         (re-search-forward regexp-end-section-js nil t)
-                         (point))))
-              (delete-region beg end)
-              (goto-char beg))
-            (goto-char (point-max)))
-        ;; Now insert protocols in user.js.
-        (insert
-         (format "// Section %s" name)
-         (format "\nuser_pref(\"network.protocol-handler.app.%s\", \"%s\");\n" name path)
-         (format "user_pref(\"network.protocol-handler.external.%s\", true);\n" name)
-         (format "// End Section %s" name))
-        (save-buffer))
-      ;; Notify mimeTypes.rdf about new handler.
-      ;; If file doesn't exists, create it and add appropriate headers
-      ;; and end balise.
-      (unless (file-exists-p firefox-default-mimeTypes-file)
-        (with-current-buffer (find-file-noselect firefox-default-mimeTypes-file)
-          (goto-char (point-min))
-          (insert
-           "<?xml version=\"1.0\"?>\n"
-           "<RDF:RDF xmlns:NC=\"http://home.netscape.com/NC-rdf#\"\n"
-           "xmlns:RDF=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n"
-           "\n</RDF:RDF>\n")
-          (nxml-mode)
-          (indent-region (point-min) (point-max))
-          (save-buffer)))
+  (let ((regexp-start-section-js  (format "// Section %s" name))
+        (regexp-end-section-js    (format "// End Section %s" name))
+        (regexp-start-section-rdf (format "<!-- Section %s -->" name))
+        (regexp-end-section-rdf   (format "<!-- End Section %s -->" name))
+        (mimeType-fname (concat (firefox-protocol-get-user-init-dir)
+                                firefox-default-mimeTypes-file))
+        (user-fname     (concat (firefox-protocol-get-user-init-dir)
+                                firefox-default-user-file)))
+    ;; Write new protocol or replace an old entry to user.js file.
+    (with-current-buffer (find-file-noselect firefox-default-user-file)
+      (goto-char (point-min))
+      ;; If entry already exists, remove it and replace by new one.
+      ;; Else we go at end of buffer and add new entry.
+      (if (re-search-forward regexp-start-section-js nil t)
+          (let ((beg (point-at-bol))
+                (end (save-excursion
+                       (re-search-forward regexp-end-section-js nil t)
+                       (point))))
+            (delete-region beg end)
+            (goto-char beg))
+          (goto-char (point-max)))
+      ;; Now insert protocols in user.js.
+      (insert
+       (format "// Section %s" name)
+       (format "\nuser_pref(\"network.protocol-handler.app.%s\", \"%s\");\n" name path)
+       (format "user_pref(\"network.protocol-handler.external.%s\", true);\n" name)
+       (format "// End Section %s" name))
+      (save-buffer))
+    ;; Notify mimeTypes.rdf about new handler.
+    ;; If file doesn't exists, create it and add appropriate headers
+    ;; and end balise.
+    (unless (file-exists-p firefox-default-mimeTypes-file)
       (with-current-buffer (find-file-noselect firefox-default-mimeTypes-file)
-        ;; If entry already exists, remove it and replace by new one.
-        ;; Else go to line 3 just after headers.
         (goto-char (point-min))
-        (if (re-search-forward regexp-start-section-rdf nil t)
-            (let ((beg (point-at-bol))
-                  (end (save-excursion
-                         (re-search-forward regexp-end-section-rdf nil t)
-                         (point))))
-              (delete-region beg end)
-              (goto-char beg))
-            (goto-char (point-min))
-            (forward-line 3))
-        ;; Now insert new handlers mime types.
-        (insert (format "\n<!-- Section %s -->\n" name))
         (insert
-         (format "<RDF:Description RDF:about=\"urn:scheme:externalApplication:%s\"\n" name)
-         (format "NC:prettyName=\"%s\"\n" name)
-         (format "NC:path=\"/home/thierry/bin/%s\" />\n" name)
-         (format "<RDF:Description RDF:about=\"urn:scheme:%s\"\n" name)
-         (format "NC:value=\"%s\">\n" name)
-         (format "<NC:handlerProp RDF:resource=\"urn:scheme:handler:%s\"/>\n" name)
-         "</RDF:Description>\n"
-         (format "<RDF:Description RDF:about=\"urn:handler:local:%s\"\n" path)
-         (format "NC:prettyName=\"%s\"\n" name)
-         (format "NC:path=\"%s\" />\n" path)
-         "\n<RDF:Seq RDF:about=\"urn:schemes:root\">\n"  
-         (format "<RDF:li RDF:resource=\"urn:scheme:%s\"/>\n" name)
-         "</RDF:Seq>\n"
-         (format "<RDF:Description RDF:about=\"urn:scheme:handler:%s\"\n" name)
-         "NC:alwaysAsk=\"false\">\n"
-         (format "<NC:externalApplication RDF:resource=\"urn:scheme:externalApplication:%s\"/>\n" name)
-         (format "<NC:possibleApplication RDF:resource=\"urn:handler:local:%s\"/>\n" path)
-         "</RDF:Description>\n"
-         (format "<!-- End Section %s -->\n" name))
+         "<?xml version=\"1.0\"?>\n"
+         "<RDF:RDF xmlns:NC=\"http://home.netscape.com/NC-rdf#\"\n"
+         "xmlns:RDF=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n"
+         "\n</RDF:RDF>\n")
         (nxml-mode)
         (indent-region (point-min) (point-max))
-        (save-buffer)))))
+        (save-buffer)))
+    (with-current-buffer (find-file-noselect firefox-default-mimeTypes-file)
+      ;; If entry already exists, remove it and replace by new one.
+      ;; Else go to line 3 just after headers.
+      (goto-char (point-min))
+      (if (re-search-forward regexp-start-section-rdf nil t)
+          (let ((beg (point-at-bol))
+                (end (save-excursion
+                       (re-search-forward regexp-end-section-rdf nil t)
+                       (point))))
+            (delete-region beg end)
+            (goto-char beg))
+          (goto-char (point-min))
+          (forward-line 3))
+      ;; Now insert new handlers mime types.
+      (insert (format "\n<!-- Section %s -->\n" name))
+      (insert
+       (format "<RDF:Description RDF:about=\"urn:scheme:externalApplication:%s\"\n" name)
+       (format "NC:prettyName=\"%s\"\n" name)
+       (format "NC:path=\"/home/thierry/bin/%s\" />\n" name)
+       (format "<RDF:Description RDF:about=\"urn:scheme:%s\"\n" name)
+       (format "NC:value=\"%s\">\n" name)
+       (format "<NC:handlerProp RDF:resource=\"urn:scheme:handler:%s\"/>\n" name)
+       "</RDF:Description>\n"
+       (format "<RDF:Description RDF:about=\"urn:handler:local:%s\"\n" path)
+       (format "NC:prettyName=\"%s\"\n" name)
+       (format "NC:path=\"%s\" />\n" path)
+       "\n<RDF:Seq RDF:about=\"urn:schemes:root\">\n"  
+       (format "<RDF:li RDF:resource=\"urn:scheme:%s\"/>\n" name)
+       "</RDF:Seq>\n"
+       (format "<RDF:Description RDF:about=\"urn:scheme:handler:%s\"\n" name)
+       "NC:alwaysAsk=\"false\">\n"
+       (format "<NC:externalApplication RDF:resource=\"urn:scheme:externalApplication:%s\"/>\n" name)
+       (format "<NC:possibleApplication RDF:resource=\"urn:handler:local:%s\"/>\n" path)
+       "</RDF:Description>\n"
+       (format "<!-- End Section %s -->\n" name))
+      (nxml-mode)
+      (indent-region (point-min) (point-max))
+      (save-buffer))))
 
 ;;; Provide
 (provide 'firefox-protocol)
