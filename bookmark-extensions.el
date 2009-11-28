5912099d237438096522fd038b601d12779e8641
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2009-11-28 09:25:19 +0100

    Improve org bookmarks.
    (bmkext-html-bookmarks-to-org): Append instead of overwriting.
    (bmkext-firefox2org-default-title, bmkext-w3m2org-default-title): New variables
    (bmkext-firefox2org-create): Use default title
    (bmkext-html-bookmarks-to-org-sync): Allow to have differents origin of bookmarks in same file.
    (bmkext-firefox2org-sync): use default title
    (bmkext-w3m2org-create, bmkext-w3m2org-sync): new functions

--- bookmark-extensions.el
+++ bookmark-extensions.el
@@ -336,7 +336,7 @@
 (eval-when-compile (require 'w3m nil t))
 (eval-when-compile (require 'w3m-bookmark nil t))
 
-(defconst bmkext-version-number "2.6.39")
+(defconst bmkext-version-number "2.6.40")
 
 (defun bmkext-version ()
   "Show version number of library `bookmark-extensions.el'."
@@ -2658,7 +2658,7 @@ ORIGIN mention where come from this bookmark."
 (defun bmkext-html-bookmarks-to-org (input-file output-file regexp &optional title-page)
   "Convert an html bookmark file to org file."
   (with-current-buffer (find-file-noselect output-file)
-    (erase-buffer) (goto-char (point-min))
+    (goto-char (point-max))
     (when title-page
       (insert (concat "* " title-page "\n\n")))
     (loop
@@ -2669,14 +2669,31 @@ ORIGIN mention where come from this bookmark."
           (bmkext-format-html-bmk-to-org i))
          (save-buffer))))
   
+
+(defvar bmkext-firefox2org-default-title "* Firefox Bookmarks")
+(defvar bmkext-w3m2org-default-title "* W3m Bookmarks")
+
 ;;;###autoload
-(defun bmkext-firefox2org-create (org-file title)
-  "Print Firefox bookmarks to ORG-FILE."
-  (interactive "sOrgFile: \nsTitlePage: ")
+(defun bmkext-firefox2org-create (org-file)
+  "Print Firefox bookmarks to ORG-FILE.
+That append all bookmarks to file if file exists.
+Use `bmkext-firefox2org-sync' instead if you want to update."
+  (interactive "sOrgFile: ")
   (bmkext-html-bookmarks-to-org (bmkext-guess-firefox-bookmark-file)
                                 org-file
                                 bmkext-firefox-bookmark-url-regexp
-                                title))
+                                bmkext-firefox2org-default-title))
+
+;;;###autoload
+(defun bmkext-w3m2org-create (org-file)
+  "Print w3m bookmarks to ORG-FILE.
+That append all bookmarks to file if file exists.
+Use `bmkext-w3m2org-sync' instead if you want to update."
+  (interactive "sOrgFile: ")
+  (bmkext-html-bookmarks-to-org w3m-bookmark-file
+                                org-file
+                                bmkext-w3m-bookmark-url-regexp
+                                bmkext-w3m2org-default-title))
 
 (defun bmkext-html-bookmarks-to-org-sync (input-file
                                           output-file
@@ -2688,28 +2705,37 @@ See `bmkext-firefox-bookmark-url-regexp', `bmkext-w3m-bookmark-url-regexp'.
 START-TITLE is the name with star where we will start (e.g * Firefox Bookmarks)."
   (with-current-buffer (find-file-noselect output-file)
     (goto-char (point-min))
+    (if start-title
+        (progn (search-forward start-title) (forward-line 1))
+        (forward-line 1))
     (org-mode)
-    (when start-title (search-forward start-title) (forward-line 1))
-    (let (org-old org-new)
-      (save-excursion ; 
-        (while (search-forward "** " nil t)
-          (push (concat
-                 (buffer-substring-no-properties (point-at-bol) (point-at-eol)) "\n")
-                org-old)))
-      (setq org-new (loop ; container of all current elm of firefox bmks formatted for org
+    (let ((beg (point))
+          (end (or (when (re-search-forward "^\*\\{1\\} " nil t)
+                     (forward-line -2) (point))
+                   (point-max)))
+          org-old org-new)
+      (goto-char beg)
+      (save-restriction
+        (narrow-to-region beg end)
+        (save-excursion ; List all currents bookmarks in this org node. 
+          (while (search-forward "** " nil t)
+            (push (concat
+                   (buffer-substring-no-properties (point-at-bol) (point-at-eol)) "\n")
+                  org-old)))
+        (setq org-new (loop ; New html bmks formatted for org
                          with alist = (bmkext-html-bookmarks-to-alist input-file regexp)
                          for i in alist
                          collect (bmkext-format-html-bmk-to-org i)))
-       (loop
-          for lnk in org-new
-          if (member lnk org-old) ; Old bookmark already exists.
-          do (search-forward "** " nil t) ; We skip it and go forward.
-          else ; Not found in ORG-OLD it's a new one we print it.
-          do
-            (forward-line 0) ; Insert from beg of line.
-            (insert lnk)
-            (search-forward "** " nil t))
-       (save-buffer))))
+        (loop
+           for lnk in org-new
+           if (member lnk org-old) ; Old bookmark already exists.
+           do (search-forward "** " nil t) ; We skip it and go forward.
+           else ; Not found in ORG-OLD it's a new one we print it.
+           do
+             (forward-line 0)
+             (insert lnk)
+             (search-forward "** " nil t))
+        (save-buffer)))))
 
 ;;;###autoload
 (defun bmkext-firefox2org-sync (org-file)
@@ -2717,7 +2743,17 @@ START-TITLE is the name with star where we will start (e.g * Firefox Bookmarks).
   (interactive "sOrgFile: ")
   (bmkext-html-bookmarks-to-org-sync (bmkext-guess-firefox-bookmark-file)
                                      org-file
-                                     bmkext-firefox-bookmark-url-regexp))
+                                     bmkext-firefox-bookmark-url-regexp
+                                     bmkext-firefox2org-default-title))
+
+;;;###autoload
+(defun bmkext-w3m2org-sync (org-file)
+  "Synchronize w3m bookmarks with ORG-FILE."
+  (interactive "sOrgFile: ")
+  (bmkext-html-bookmarks-to-org-sync w3m-bookmark-file
+                                     org-file
+                                     bmkext-w3m-bookmark-url-regexp
+                                     bmkext-w3m2org-default-title))
 
 ;;; GNUS support.  Does not handle regions.
 (defun bmkext-make-gnus-record ()
