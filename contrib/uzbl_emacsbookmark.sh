cfc6ba7c4a0a8652d44ae336ddc9726e7184d120
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2011-06-18 16:13:13 +0200

    * contrib/README improve * contrib/emacsuzblbookmarks Change headers * contrib/uzbl_emacsbookmark.sh Remove zenity stuff

--- contrib/uzbl_emacsbookmark.sh
+++ contrib/uzbl_emacsbookmark.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
+## Title:
+## Description: Record a bookmark in emacs-bookmarks from uzbl. 
+## Author:Thierry Volpiatto<thierry dot volpiatto FROM gmail DOT com>
+## Commentary:
 
-which zenity 2>&1 >/dev/null || exit 2
-url=$6
-# replace tabs, they are pointless in titles and we want to use tabs as delimiter.
-title=$(echo "$7" | sed 's/\t/    /')
-
-entry=`zenity --entry --text="Add Bookmark." --entry-text="$url $title\t"`
-exitstatus=$?
-if [ $exitstatus -ne 0 ]; then exit $exitstatus; fi
-
-url=$(echo $entry | awk '{print $1}')
+url="$6"
+title="$7"
 
 wmctrl -xa emacs
 
